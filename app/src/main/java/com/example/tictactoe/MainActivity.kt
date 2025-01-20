7ec0c22576794f0f7cfc1fe84162ac6a9abbfaaf
ronenanuka6 <ronenanuka6@gmail.com>
2025-01-20 21:19:14 +0200

    ronen

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import android.util.Log
+import android.util.Xml
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    enum class Turn
+    {
+        O,
+        X
+    }
+
+    private var firstTurn = Turn.X
+    private var currentTurn = Turn.X
+
+    private var xScore = 0
+    private var oScore = 0
+
+    private var boardList = mutableListOf<Button>()
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        initBoard()
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun initBoard() {
+        boardList.add(binding.a1)
+        boardList.add(binding.a2)
+        boardList.add(binding.a3)
+        boardList.add(binding.b1)
+        boardList.add(binding.b2)
+        boardList.add(binding.b3)
+        boardList.add(binding.c1)
+        boardList.add(binding.c2)
+        boardList.add(binding.c3)
+    }
+}
